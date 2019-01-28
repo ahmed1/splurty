93a05dcf3f279adc5ac242e819c77d4148fb659a
Ahmed <ahmedshoukr2@gmail.com>
2019-01-27 19:11:44 -0800

    added 2 quotes and a random quote generator

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,4 +1,8 @@
 class QuotesController < ApplicationController
     def index
+        @quote = Quote.order("Random()").first #  pull the very first quote from our database and store it in a variable
+        #Now, the first quote that we have in our database is stuffed into the variable called @quote
+        
+        @quote2 = Quote.second
     end
 end
