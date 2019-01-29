c49e87cff674985fe22a4144e3b0c3c3314db6eb
Ahmed <ahmedshoukr2@gmail.com>
2019-01-29 00:06:37 -0800

    added new quotes form page w/link from contribute

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,11 @@ class QuotesController < ApplicationController
     def index
         @quote = Quote.order("Random()").first #  pull the very first quote from our database and store it in a variable
         #Now, the first quote that we have in our database is stuffed into the variable called @quote
-        
-        @quote2 = Quote.second
     end
+
+    def new
+      @quote = Quote.new
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'quotes#index'
+  resources :quotes # Adding the resources :quotes line of code gave us a standard set of URLs to Create, Read, Update and Destroy (aka CRUD) for our quotes.
 end
